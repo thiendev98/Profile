b88053a5eb63135e32417fe5a4f78e6c574c4aa6
hoangthien <thien.dohoangcs98@gmail.com>
2022-04-27 13:46:30 +0700

    update controller

--- src/Controller.js
+++ src/Controller.js
@@ -1,31 +1,140 @@
 import React, { useState } from "react";
 import Home from "./components/Home";
 import Product from "./components/Product";
-import Contact from "./components/Contact";
-import About from "./components/About";
+import Contact from "./components/Hobby";
+import About from "./components/Target";
+import $ from "jquery";
+import "./components/styles/style.css";
+import {
+  FaFacebookMessenger,
+  FaFacebook,
+  FaGithub,
+  FaInstagram,
+} from "react-icons/fa";
+import avatar from "./components/images/avatar.jpg";
+import user from "./components/images/user.gif";
+import book from "./components/images/book.gif";
+import heart from "./components/images/heart.gif";
+import target from "./components/images/target.gif";
+import Target from "./components/Target";
+import Hobby from "./components/Hobby";
 const PAGE_HOME = "home";
-const PAGE_CONTACT = "contact";
+const PAGE_HOBBY = "hobby";
 const PAGE_PRODUCT = "product";
-const PAGE_ABOUT = "about";
+const PAGE_TARGET = "target";
 
 export default function Controller() {
   const [page, setPage] = useState(PAGE_HOME);
   const navigateTo = (nextPage) => {
     setPage(nextPage);
   };
-
+  const infoList = [
+    {
+      title: "Email",
+      content: "thien.dohoangcs98@gmail.com",
+    },
+    {
+      title: "Ngày sinh",
+      content: "28 - 05 - 1998",
+    },
+    {
+      title: "Số điện thoại",
+      content: "0865676490",
+    },
+    {
+      title: "Quê quán",
+      content: "Phước Long - Bạc Liêu",
+    },
+  ];
+  const sidebarList = [
+    {
+      title: "Giới thiệu",
+      image: user,
+      page: PAGE_HOME,
+    },
+    {
+      title: "Sở thích",
+      image: heart,
+      page: PAGE_HOBBY,
+    },
+    {
+      title: "Sản phẩm",
+      image: book,
+      page: PAGE_PRODUCT,
+    },
+    {
+      title: "Mục tiêu",
+      image: target,
+      page: PAGE_TARGET,
+    },
+  ];
   return (
     <div id="controller">
+      <marquee onmouseover="this.stop()" onmouseout="this.start()">
+        Chào mừng bạn đến với Website của tôi...
+      </marquee>
       <header>
-        <li onClick={() => navigateTo(PAGE_HOME)}>Home</li>
-        <li onClick={() => navigateTo(PAGE_ABOUT)}>About</li>
-        <li onClick={() => navigateTo(PAGE_PRODUCT)}>Product</li>
-        <li onClick={() => navigateTo(PAGE_CONTACT)}>Contact</li>
+        <div className="header">
+          <div className="row">
+            <div className="col-2">
+              <img className="header__avatar" src={avatar} alt="avatar" />
+            </div>
+            <div className="col-4 row">
+              <h4 className="header__name">Đỗ Hoàng Thiên</h4>
+              <p className="header__about">Frontend Developer</p>
+              <ul className="header__list">
+                <li className="header__link">
+                  <FaFacebook className="icon" />
+                </li>
+                <li className="header__link">
+                  <FaFacebookMessenger className="icon" />
+                </li>
+                <li className="header__link">
+                  <FaInstagram className="icon" />
+                </li>
+                <li className="header__link">
+                  <FaGithub className="icon" />
+                </li>
+              </ul>
+            </div>
+            <div className="row col-6">
+              {infoList.map((info) => (
+                <div className="col-6">
+                  <h5 className="infoList__title">{info.title}</h5>
+                  <p className="infoList__content">{info.content}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </header>
-      {page === PAGE_HOME && <Home />}
-      {page === PAGE_CONTACT && <Contact />}
-      {page === PAGE_ABOUT && <About />}
-      {page === PAGE_PRODUCT && <Product />}
+      <section>
+        <div className="row">
+          <div className="col-2">
+            <div className="sidebar">
+              <div className="sidebar__list">
+                {sidebarList.map((list) => (
+                  <div
+                    className="sidebar__link"
+                    onClick={() => navigateTo(list.page)}
+                  >
+                    <img className="sidebar__img" src={list.image} />
+                    <span>{list.title}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="col-10">
+            <div className="content">
+              {page === PAGE_HOME && <Home />}
+              {page === PAGE_TARGET && <Target />}
+              {page === PAGE_HOBBY && <Hobby />}
+              {page === PAGE_PRODUCT && <Product />}
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,12 @@
 import React from "react";
-
+import "./styles/style.css";
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div id="HomePage">
+      <div className="row">
+        <div className="col-6">AAAA</div>
+        <div className="col-6">b</div>
+      </div>
+    </div>
+  );
 }
