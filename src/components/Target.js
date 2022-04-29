5ee3f24ff503e2c441cf249844bc68d047488fe0
hoangthien <thien.dohoangcs98@gmail.com>
2022-04-29 10:07:19 +0700

    update

--- src/components/Target.js
+++ src/components/Target.js
@@ -1,5 +1,52 @@
 import React from "react";
-
 export default function Target() {
-  return <div>Target</div>;
+  const shorts = [
+    {
+      key: 1,
+      content:
+        "Rèn luyện bản thân, tích cực học tập, tham gia các hoạt động phong trào do trường, khoa tổ chức",
+    },
+    {
+      key: 2,
+      content: "Cũng cố các kiến thức nền tảng về HTML, CSS, JavaScript",
+    },
+    {
+      key: 3,
+      content: "Tìm hiểu thêm kiến thức nâng cao về HTML, CSS, JavaScript",
+    },
+    {
+      key: 4,
+      content:
+        "Ôn tập, tìm hiểu thêm các tính năng của Bootstrap, Jquery, ReactJS",
+    },
+    {
+      key: 5,
+      content:
+        "Tìm được nơi thực tập phù hợp với bản thân để có thể học hỏi kinh nghiệm từ những anh, chị đi trước",
+    },
+  ];
+  const longs = [
+    {
+      key: 1,
+      content: "Tốt nghiệp đúng hạn",
+    },
+    {
+      key: 2,
+      content: "Học cách sử dụng nền tảng (Platform) Nodejs",
+    },
+    {
+      key: 3,
+      content: "Ôn tập, tìm hiểu thêm kiến về ngôn ngữ PHP",
+    },
+    {
+      key: 4,
+      content: "Hướng tới mục tiêu trở thành lập trình viên Fullstack",
+    },
+  ];
+
+  return (
+    <div id="TargetPage">
+      <h1>Target</h1>
+    </div>
+  );
 }
